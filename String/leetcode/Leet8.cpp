c6940e7a83fa942e9646112d50a79c1416c5537e
Karan Kumar Sah <karankumarsahtkg2018@gmail.com>
2024-10-18 01:42:09 +0530

    Refactor myAtoi function in Leet8.cpp to handle larger numbers

--- String/leetcode/Leet8.cpp
+++ String/leetcode/Leet8.cpp
@@ -5,22 +5,20 @@ class Solution {
 public:
     int myAtoi(string s) {
         int i = 0;
-        while (i < s.size() && s[i] == ' ') {
+        while (i < s.size() && s[i] == ' ') 
             i++;
-        }
         int op = 1;
-        if (i < s.size() && (s[i] == '-' || s[i] == '+')) {
+        if ((s[i] == '-' || s[i] == '+')) {
             op = (s[i] == '-') ? -1 : 1;
             i++;
         }
-        int ans = 0;
+        long long ans = 0;
         while(s[i] - '0' <= 9 && s[i] - '0' >= 0 ){
             ans = ans * 10 + (s[i] - '0');
             if (ans * op >= INT_MAX) return INT_MAX;
             if (ans * op <= INT_MIN) return INT_MIN;
-            
             i++;
         }
-        return op * ans;
+        return (int)op * ans;
     }
 };
