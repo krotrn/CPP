9444af02fb85b98a732d8da8f03841eba3acabe0
Karan Kumar Sah <karankumarsahtkg2018@gmail.com>
2024-11-09 23:03:34 +0530

    fix:Correction

--- 2DArray/LeetCode/leet861.cpp
+++ 2DArray/LeetCode/leet861.cpp
@@ -1,28 +1,54 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-void chr(vector<vector<int>> &v,int x,int y){
-    for (int i = 0; i < y; i++){
-        if(v[x][i]==0) v[x][i] = 1;
-        else v[x][i] = 0;
-    }
-}
-
-void chc(vector<vector<int>> &v,int x,int y){
-    for (int i = 0; i < x; i++){
-        if(v[i][y]==0) v[i][y] = 1;
-        else v[i][y] = 0;
-    }
-}
-
-void display(vector<vector<int>> &v,int x,int y){
-    for (int i = 0; i < x;i++){
-        for (int j = 0; j < y; j++){
-            cout << v[i][j] << " ";
+class Solution {
+    void chr(vector<vector<int>> &v,int x,int y){
+        for (int i = 0; i < y; i++){
+            if(v[x][i]==0) v[x][i] = 1;
+            else v[x][i] = 0;
         }
-            cout << endl;
     }
-}
+
+    void chc(vector<vector<int>> &v,int x,int y){
+        for (int i = 0; i < x; i++){
+            if(v[i][y]==0) v[i][y] = 1;
+            else v[i][y] = 0;
+        }
+    }
+    int sum(vector<vector<int>> &v,int r,int c){
+        int sum = 0;
+        for (int i = 0; i<r; i++){
+            int x = 1;
+            for(int j = c-1; j>=0; j--){
+                sum += v[i][j]*x;
+                x *= 2;
+            }
+        }
+        return sum;
+    }
+public:
+    int matrixScore(vector<vector<int>>& grid) {
+        int r = grid.size(), c = grid[0].size();
+        for (int i = 0; i < r;i++){
+            if(grid[i][0]==0){
+                chr(grid, i, c);
+            }
+        }
+
+        for(int j=0; j<c; j++){
+            int noz=0,noo=0;
+            for(int i=0; i<r; i++){
+                if(grid[i][j]==0) noz++;
+                else noo++;
+            }
+            if(noz>noo){
+                chc(grid, r, j);
+            }
+        }
+
+        return sum(grid,r,c);
+    }
+};
 
 int main(){
     cout << "Enter rows and coloumn : ";
@@ -36,17 +62,8 @@ int main(){
         }
     }
 
-    for (int i = 0; i < r;i++){
-        if(v[i][0]==0){
-            chr(v, i, c);
-        }
-    }
-
-    for (int i = 0; i < c;i++){
-        if(v[0][i]==0){
-            chc(v, r, i);
-        }
-    }
+    Solution s;
+    cout << s.matrixScore(v) << endl;
 
     display(v, r, c);
 }
