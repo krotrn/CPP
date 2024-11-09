16657cb880f1e728a3a9bf3ad7bd633c310329e0
Karan Kumar Sah <karankumarsahtkg2018@gmail.com>
2024-11-10 00:42:29 +0530

    refactor: improve code readability and structure in Leet42.cpp and leet31.cpp

--- Arrays/leetcode/Leet42.cpp
+++ Arrays/leetcode/Leet42.cpp
@@ -1,91 +1,90 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
-int main(){
-        /*class Solution {
+class Solution
+{
 public:
-    int trap(vector<int>& height) {
-        int m=height[0],c=0,q=height.size();
+    int trap(vector<int> &height)
+    {
+        int m = height[0], c = 0, q = height.size();
         vector<int> v1(q);
         vector<int> v2(q);
-        v1[0]=-1,v2[q-1]=-1;
-        for(int i=1; i<q; i++){
-            if(m<height[i])m=height[i];
-            v1[i]=m;
+        v1[0] = -1, v2[q - 1] = -1;
+
+        for (int i = 1; i < q; i++)
+        {
+            if (m < height[i])
+                m = height[i];
+            v1[i] = m;
         }
-        m=height[q-1];
-        for(int i=q-2; i>=0; i--){
-            if(m<height[i]) m=height[i];
-            v2[i]=m;
+        m = height[q - 1];
+        for (int i = q - 2; i >= 0; i--)
+        {
+            if (m < height[i])
+                m = height[i];
+            v2[i] = m;
         }
         vector<int> v3(q);
-        for(int i=0;i<q;i++){
-            v3[i]=min(v1[i],v2[i]);
+        for (int i = 0; i < q; i++)
+        {
+            v3[i] = min(v1[i], v2[i]);
         }
-        for(int i=1;i<q-1;i++){
-            if(height[i]<v3[i]){
-                c=c + v3[i]-height[i];
+        for (int i = 1; i < q - 1; i++)
+        {
+            if (height[i] < v3[i])
+            {
+                c = c + v3[i] - height[i];
             }
         }
-    return c;
+        return c;
     }
-};*/
+};
 
-
-
-
-/*class Solution {
+// 2nd approach (2 pointer approach)
+class Solution{
 public:
     int trap(vector<int>& height) {
-        int m=height[0],c=0,q=height.size();
-        vector<int> v1(q);
-        vector<int> v2(q);
-        v1[0]=-1,v2[q-1]=-1;
-        for(int i=1; i<q; i++){
-            if(m<height[i])m=height[i];
-            v1[i]=m;
-        }
-        m=height[q-1];
-        for(int i=q-2; i>=0; i--){
-            if(m<height[i]) m=height[i];
-            v2[i]=m;
-        }
-        for(int i=0;i<q;i++){
-            v1[i]=min(v1[i],v2[i]);
-        }
-        for(int i=1;i<q-1;i++){
-            if(height[i]<v1[i]){
-                c=c + v1[i]-height[i];
+        int ans = 0;
+        int left = 0, right = height.size() - 1;
+        int left_max = 0, right_max = 0;
+        while(left < right){
+            // if left height is smaller than right height
+            // then we can trap water at left side
+            if(height[left] <= height[right]){
+                // if left height is greater than left_max
+                // then update left_max
+                // else add the difference between left_max and left height to ans
+                if(height[left] >= left_max)
+                    left_max = height[left];
+                else
+                    ans += left_max - height[left];
+            }
+                // if right height is greater than left height
+                // then we can trap water at right side
+            else{
+                // if right height is greater than right_max  
+                // then update right_max
+                // else add the difference between right_max and right height to ans
+                if(height[right] >= right_max)
+                    right_max = height[right];
+                else
+                    ans += right_max - height[right];
             }
         }
-    return c;
+        return ans;
+        
     }
-};*/
+};
 
-
-
-
-/*class Solution {
-public:
-    int trap(vector<int>& height) {
-        int m=height[0],c=0,q=height.size();
-        vector<int> v1(q);
-        v1[0]=-1;
-        for(int i=1; i<q; i++){
-            if(m<height[i])m=height[i];
-            v1[i]=m;
-        }
-        m=height[q-1];
-        for(int i=q-2; i>=0; i--){
-            if(m < v1[i]) v1[i]=m;
-            if(m<height[i]) m=height[i];
-        }
-        for(int i=1;i<q-1;i++){
-            if(height[i]<v1[i]){
-                c=c + v1[i]-height[i];
-            }
-        }
-    return c;
-    }
-};*/
+int main()
+{
+    Solution s;
+    vector<int> v = {0,1,0,2,1,0,1,3,2,1,2,1};
+    cout << s.trap(v);
 }
+
+static const auto speedup = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+};
--- Arrays/leetcode/leet31.cpp
+++ Arrays/leetcode/leet31.cpp
@@ -1,43 +1,43 @@
 #include<iostream>
 #include<vector>
-int main(){
-    //find pivoted index(pivot = first element doesnot follow accending order from last)
+//https://leetcode.com/media/original_images/31_Next_Permutation.gif
+class Solution {
     //sort all element after pivot index
     //swap pivot element and just largest then pivit element
-    /*class Solution {
-        public:
-        void nextPermutation(vector<int>& nums) {
-            int idx=-1;
-            int n = nums.size();
-            for(int i = n-2; i>=0; i--){
-            if(nums[i]<nums[i+1]){
-                    idx = i;
-                    break;
-                }
+    //find pivoted index(pivot = first element doesnot follow accending order from last)
+    public:
+    void nextPermutation(vector<int>& nums) {
+        int idx=-1;
+        int n = nums.size();
+        for (int i = n - 2; i >= 0; i--){
+            if (nums[i] < nums[i + 1]){
+                idx = i;
+                break;
             }
-            if(idx==-1){
+        }
+        if(idx==-1){
             reverse(nums.begin(),nums.end());
             return;
+        }
+        reverse(nums.begin()+idx+1,nums.end());
+        for(int i=idx+1;i<n;i++){
+            if(nums[idx]<nums[i]){
+                n=i;
             }
-            reverse(nums.begin()+idx+1,nums.end());
-            for(int i=idx+1;i<n;i++){
-                if(nums[idx]<nums[i]){
-                    n=i;
-                }
-            }
-            int t=nums[idx];
-            nums[idx]=nums[n];
-            nums[n]=t;
+        }
+        int t=nums[idx];
+        nums[idx]=nums[n];
+        nums[n]=t;
         return;
-        }
-    };*/
-    //https://leetcode.com/media/original_images/31_Next_Permutation.gif
+    }
+};
     //use function
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        next_permutation(nums.begin(), nums.end());
+    }
+};
+int main(){
 
-    /*class Solution {
-    public:
-        void nextPermutation(vector<int>& nums) {
-            next_permutation(nums.begin(), nums.end());
-        }
-    };*/
 }
