f22961b63178a220bd993a84c1b1f73df3aa41f0
Karan Kumar Sah <karankumarsahtkg2018@gmail.com>
2024-11-10 18:14:55 +0530

    refactor: add comments for clarity in Leet1331.cpp

--- LeetCode/Leet1331.cpp
+++ LeetCode/Leet1331.cpp
@@ -7,9 +7,11 @@ public:
     vector<int> arrayRankTransform(vector<int>& arr) {
         if (arr.empty())
             return arr;
+        // rank[i] = {arr[i], i}
         vector<pair<int, int>> rank(arr.size(), {0, 0});
         for (int i = 0; i < arr.size(); i++)
             rank[i] = {arr[i], i};
+        // sort rank array based on arr[i]
         sort(rank.begin(), rank.end());
         arr[rank[0].second] = 1;
         for (int i = 1; i < arr.size(); i++)
