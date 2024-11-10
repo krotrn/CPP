4e1c9ac59f4711641dc74d67cdcf650cfa0222e0
Karan Kumar Sah <karankumarsahtkg2018@gmail.com>
2024-11-10 18:13:36 +0530

    saprate folder for leetcode practice

---
+++ LeetCode/Leet1331.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> arrayRankTransform(vector<int>& arr) {
+        if (arr.empty())
+            return arr;
+        vector<pair<int, int>> rank(arr.size(), {0, 0});
+        for (int i = 0; i < arr.size(); i++)
+            rank[i] = {arr[i], i};
+        sort(rank.begin(), rank.end());
+        arr[rank[0].second] = 1;
+        for (int i = 1; i < arr.size(); i++)
+            arr[rank[i].second] = (rank[i].first == rank[i - 1].first) ? arr[rank[i - 1].second] : (arr[rank[i - 1].second] + 1);
+        return arr;
+    }
+};
