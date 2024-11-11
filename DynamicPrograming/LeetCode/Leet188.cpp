2d2ff0c646171f276f6fac70b5e99f462300d202
Karan Kumar Sah <karankumarsahtkg2018@gmail.com>
2024-11-11 15:17:27 +0530

    feat: implement iterative solution for the Stock Buy and Sell problem with k transactions

--- DynamicPrograming/LeetCode/Leet188.cpp
+++ DynamicPrograming/LeetCode/Leet188.cpp
@@ -2,27 +2,67 @@
 
 using namespace std;
 
-class Solution{
-    int helper(int k, vector<int> &prices, int idx, bool ongoing, vector<vector<vector<int>>> &dp){
-        if(idx == prices.size())
+class Solution {
+    // Approach: 1 Recursive
+    int helper(int k, vector<int>& prices, int idx, bool ongoing,
+               vector<vector<vector<int>>>& dp) {
+        if (idx == prices.size() || k == 0)
             return 0;
         // avoid
-        if(dp[idx][k][ongoing] != -1)
+        if (dp[idx][k][ongoing] != -1)
             return dp[idx][k][ongoing];
         dp[idx][k][ongoing] = helper(k, prices, idx + 1, ongoing, dp);
-        if(ongoing)
-            return dp[idx][k][ongoing] = max(dp[idx][k][ongoing], prices[idx] + helper(k - 1, prices, idx + 1, false, dp));
-        else {
-            if(k)
-                return dp[idx][k][ongoing] = max(dp[idx][k][ongoing], helper(k - 1, prices, idx + 1, true, dp) - prices[idx]);
-        }
-        return dp[idx][k][ongoing]; 
+        if (ongoing)
+            dp[idx][k][ongoing] =
+                max(dp[idx][k][ongoing],
+                    prices[idx] + helper(k - 1, prices, idx + 1, false, dp));
+        else
+            dp[idx][k][ongoing] =
+                max(dp[idx][k][ongoing],
+                    helper(k, prices, idx + 1, true, dp) - prices[idx]);
+        return dp[idx][k][ongoing];
     }
+
+    // Approach: 2 Iterative
+    int helper2(int k, vector<int>& prices, vector<vector<vector<int>>>& dp) {
+        int n = prices.size();
+
+        for (int i = 0; i <= k; i++) {
+            dp[0][i][0] = 0;          // no stocks
+            dp[0][i][1] = -prices[0]; // stocks
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j <= k; j++) {
+                // for not holding stock
+                dp[i][j][0] = max(dp[i - 1][j][0], dp[i - 1][j][1] + prices[i]);
+                // for holding stock
+                dp[i][j][1] =
+                    max(dp[i - 1][j][1], dp[i - 1][j - 1][0] - prices[i]);
+            }
+        }
+        return dp[n - 1][k][0];
+    }
+
 public:
-    int maxProfit(int k, vector<int> &prices){
-        vector<vector<vector<int>>> dp(prices.size() + 1, vector<vector<int>>(k + 1, vector<int>(2, -1)));
-        int idx = 0;
-        return helper(k, prices, idx, false, dp);
+    int maxProfit(int k, vector<int>& prices) {
+        // vector<vector<vector<int>>> dp(prices.size() + 1,
+        // vector<vector<int>>(k + 1, vector<int>(2, -1)));
+        vector<vector<vector<int>>> dp(
+            prices.size() + 1, vector<vector<int>>(k + 1, vector<int>(2, 0)));
+        if (prices.size() == 0 || k == 0)
+            return 0;
+
+        // If k is large enough, apply greedy approach
+        if (k >= prices.size() / 2) {
+            int maxProfit = 0;
+            for (int i = 1; i < prices.size(); ++i) {
+                maxProfit += max(0, prices[i] - prices[i - 1]);
+            }
+            return maxProfit;
+        }
+
+        // return helper(k, prices, 0, false, dp);
+        return helper2(k, prices, dp);
     }
 };
 
