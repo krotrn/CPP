672fb2668f69d2c1958e4fb1d553fcc56dde81ec
Karan Kumar Sah <karankumarsahtkg2018@gmail.com>
2024-11-11 19:53:28 +0530

    feat: implement solution for the Median of Sliding Window problem

--- Greedy/LeetCode/Leet480.cpp
+++ Greedy/LeetCode/Leet480.cpp
@@ -4,113 +4,82 @@ using namespace std;
 // unsolved
 
 class Solution {
+    void balanceHeaps(priority_queue<int> &left, priority_queue<int, vector<int>, greater<int>> &right, int &leftSize, int &rightSize){
+        while (leftSize > rightSize + 1) {
+            right.push(left.top());
+            left.pop();
+            leftSize--;
+            rightSize++;
+        }
+        while (rightSize > leftSize) {
+            left.push(right.top());
+            right.pop();
+            rightSize--;
+            leftSize++;
+        }
+    }
+
+    int getMedian(priority_queue<int> &left, priority_queue<int, vector<int>, greater<int>> &right, int &leftSize, int &rightSize){
+        if (leftSize > rightSize)
+                return left.top();
+            else
+                return ((double)left.top() + (double)right.top()) / 2.0;
+    }
+
 public:
-    // Function to get the median from the two heaps
-    double getMedian(priority_queue<int>& lft, priority_queue<int, vector<int>, greater<int>>& rgt, int& sz_lft, int& sz_rgt) {
-        if (sz_lft > sz_rgt) {
-            return round(lft.top() * 100000.0) / 100000.0;
-        } else {
-            return round(rgt.top() + ((lft.top() - rgt.top()) / 2.0) * 100000.0) / 100000.0;
-        }
-    }
-
-    // Function to balance the two heaps
-    void balanceHeaps(priority_queue<int>& lft, priority_queue<int, vector<int>, greater<int>>& rgt, int& sz_lft, int& sz_rgt) {
-        if (sz_lft > sz_rgt + 1) {
-            rgt.push(lft.top());
-            lft.pop();
-            sz_lft--;
-            sz_rgt++;
-        } else if (sz_rgt > sz_lft) {
-            lft.push(rgt.top());
-            rgt.pop();
-            sz_rgt--;
-            sz_lft++;
-        }
-    }
-
-    // Function to find the median of each sliding window of size k
     vector<double> medianSlidingWindow(vector<int>& nums, int k) {
-        vector<double> ans;
-        priority_queue<int> lft; // max-heap (left side)
-        priority_queue<int, vector<int>, greater<int>> rgt; // min-heap (right side)
-        int sz_lft = 0, sz_rgt = 0;
-        
-        // Initialize the heaps with the first k elements
-        for (int i = 0; i < k; i++) {
-            if (lft.empty() || nums[i] <= lft.top()) {
-                lft.push(nums[i]);
-                sz_lft++;
+        vector<double> result;
+        priority_queue<int> left; 
+        priority_queue<int, vector<int>, greater<int>> right; 
+        unordered_map<int, int> delay; 
+        int leftSize = 0, rightSize = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            // Add new number
+            if (left.empty() || nums[i] <= left.top()) {
+                left.push(nums[i]);
+                leftSize++;
             } else {
-                rgt.push(nums[i]);
-                sz_rgt++;
-            }
-            balanceHeaps(lft, rgt, sz_lft, sz_rgt);
-        }
-        
-        // Add the median of the first window to the result
-        double median = getMedian(lft, rgt, sz_lft, sz_rgt);
-        stringstream ss;
-        ss << fixed << setprecision(5) << median;
-        double formatted_median;
-        ss >> formatted_median;
-        ans.push_back(formatted_median);
-
-        unordered_map<int, int> delay; // Map to keep track of elements to be removed
-        
-        // Process the rest of the elements
-        for (int i = k; i < nums.size(); i++) {
-            int newEle = nums[i];
-            int oldEleToDel = nums[i - k];
-
-            // Add the new element to the appropriate heap
-            if (newEle <= lft.top()) {
-                lft.push(newEle);
-                sz_lft++;
-            } else {
-                rgt.push(newEle);
-                sz_rgt++;
-            }
-            balanceHeaps(lft, rgt, sz_lft, sz_rgt);
-
-            // Mark the old element for delayed removal
-            delay[oldEleToDel]++;
-
-            if(oldEleToDel <= getMedian(lft, rgt, sz_lft, sz_rgt)) {
-                sz_lft--;
-            } else {
-                sz_rgt--;
+                right.push(nums[i]);
+                rightSize++;
             }
 
-            // Remove elements from the left heap that are marked for delayed removal
-            while (!lft.empty() && delay.count(lft.top())) {
-                delay[lft.top()]--;
-                if (delay[lft.top()] == 0) {
-                    delay.erase(lft.top());
+            // Balance heaps
+            balanceHeaps(left,right,leftSize,rightSize);
+
+            // Remove the number outside the sliding window
+            if (i >= k) {
+                int toRemove = nums[i - k];
+                delay[toRemove]++;
+
+                // Adjust heap sizes for delayed removal
+                if (toRemove <= left.top()) leftSize--;
+                else rightSize--;
+
+                // Clean up delayed elements in heaps
+                while (!left.empty() && delay[left.top()]) {
+                    delay[left.top()]--;
+                    left.pop();
                 }
-                lft.pop();
-            }
-            
-            // Remove elements from the right heap that are marked for delayed removal
-            while (!rgt.empty() && delay.count(rgt.top())) {
-                delay[rgt.top()]--;
-                if (delay[rgt.top()] == 0) {
-                    delay.erase(rgt.top());
+                while (!right.empty() && delay[right.top()]) {
+                    delay[right.top()]--;
+                    right.pop();
                 }
-                rgt.pop();
+
+                // Rebalance heaps after cleanup
+                balanceHeaps(left,right,leftSize,rightSize);
             }
 
-            // Balance the heaps again after removals
-            balanceHeaps(lft, rgt, sz_lft, sz_rgt);
-            // Add the median of the current window to the result
-            median = getMedian(lft, rgt, sz_lft, sz_rgt);
-            ss << fixed << setprecision(5) << median;
-            ss >> formatted_median;
-            ans.push_back(formatted_median);
+            // Add median to result if window is full
+            if (i >= k - 1) {
+                result.push_back(getMedian(left,right,leftSize,rightSize));
+            }
         }
-        return ans;
+
+        return result;
     }
 };
+
 
 int main(){
     Solution s;
